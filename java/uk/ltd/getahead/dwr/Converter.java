94c98a949b43facbcff2b61a5a71ee97cff44139
Joe Walker <joe at getahead dot org>
2008-03-27 16:45:18 +0000

    Moving tags into DWr project

---
+++ java/uk/ltd/getahead/dwr/Converter.java
@@ -0,0 +1,45 @@
+package uk.ltd.getahead.dwr;
+
+/**
+ * An interface for converting types from a string to some other type.
+ * @author Joe Walker [joe at getahead dot ltd dot uk]
+ */
+public interface Converter
+{
+    /**
+     * If we are a compound converter that farms out part of the conversion
+     * to other converters then you farm the conversion out via a configuration.
+     * @param config The confiuration object
+     */
+    public void setConverterManager(ConverterManager config);
+
+    /**
+     * Attempt to coerce the data from a string to an Object.
+     * If anything goes wrong with inbound conversion then we except and die
+     * because we are converting data from the untrusted internet so we take the
+     * assumption that anything wrong is someone having a go and throw.
+     * @param paramType The type to convert to
+     * @param data The data to convert
+     * @param inctx The map of data that we are working on
+     * @return The convered data, or null if the conversion was not possible
+     * @throws ConversionException If the conversion failed for some reason
+     */
+    public Object convertInbound(Class paramType, InboundVariable data, InboundContext inctx) throws ConversionException;
+
+    /**
+     * Return a javascript string that defines the variable named varName to
+     * have the contents of the converted object data.
+     * In contrast to <code>convertInbound()</code> any failures in converting
+     * data on the way out should not stop processing, and we should carry on
+     * if we can. Failures are probably down to some misconfiguration so as much
+     * information about the error as can be safely generated to console logs is
+     * good. In other words if you need to loop in outbound conversion then it
+     * might be a good idea to catch issues inside the loop, log, and carry on.
+     * @param data The data to convert
+     * @param varName The variable to define
+     * @param outctx Objects already converted and the results
+     * @return The string that defines the given variable
+     * @throws ConversionException If the conversion failed for some reason
+     */
+    public String convertOutbound(Object data, String varName, OutboundContext outctx) throws ConversionException;
+}
---
+++ java/uk/ltd/getahead/dwr/convert/JDOMConverter.java
@@ -0,0 +1,125 @@
+package uk.ltd.getahead.dwr.convert;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+import uk.ltd.getahead.dwr.ConversionException;
+import uk.ltd.getahead.dwr.Converter;
+import uk.ltd.getahead.dwr.ConverterManager;
+import uk.ltd.getahead.dwr.InboundContext;
+import uk.ltd.getahead.dwr.InboundVariable;
+import uk.ltd.getahead.dwr.Messages;
+import uk.ltd.getahead.dwr.OutboundContext;
+import uk.ltd.getahead.dwr.util.JavascriptUtil;
+import uk.ltd.getahead.dwr.util.LocalUtil;
+
+/**
+ * An implementation of Converter for DOM objects.
+ * @author Joe Walker [joe at eireneh dot com]
+ * @version $Id: StringConverter.java,v 1.2 2004/11/04 15:54:07 joe_walker Exp $
+ */
+public class JDOMConverter implements Converter
+{
+    /* (non-Javadoc)
+     * @see uk.ltd.getahead.dwr.Converter#init(uk.ltd.getahead.dwr.DefaultConfiguration)
+     */
+    public void setConverterManager(ConverterManager newConfig)
+    {
+    }
+
+    /* (non-Javadoc)
+     * @see uk.ltd.getahead.dwr.Converter#convertInbound(java.lang.Class, java.util.List, uk.ltd.getahead.dwr.InboundVariable, uk.ltd.getahead.dwr.InboundContext)
+     */
+    public Object convertInbound(Class paramType, InboundVariable iv, InboundContext inctx) throws ConversionException
+    {
+        String value = LocalUtil.decode(iv.getValue());
+
+        try
+        {
+            SAXBuilder builder = new SAXBuilder();
+            Document doc = builder.build(new StringReader(value));
+
+            if (paramType == Document.class)
+            {
+                return doc;
+            }
+            else if (paramType == Element.class)
+            {
+                return doc.getRootElement();
+            }
+
+            throw new ConversionException(Messages.getString("DOMConverter.UnusableClass", paramType.getName())); //$NON-NLS-1$
+        }
+        catch (ConversionException ex)
+        {
+            throw ex;
+        }
+        catch (Exception ex)
+        {
+            throw new ConversionException(ex);
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see uk.ltd.getahead.dwr.Converter#convertOutbound(java.lang.Object, java.lang.String, uk.ltd.getahead.dwr.OutboundContext)
+     */
+    public String convertOutbound(Object data, String varname, OutboundContext outctx) throws ConversionException
+    {
+        try
+        {
+            Format outformat = Format.getCompactFormat();
+            outformat.setEncoding("UTF-8"); //$NON-NLS-1$
+
+            // Setup the destination
+            StringWriter xml = new StringWriter();
+
+            XMLOutputter writer = new XMLOutputter(outformat);
+
+            // Using XSLT to convert to a stream. Setup the source
+            if (data instanceof Document)
+            {
+                Document doc = (Document) data;
+                writer.output(doc, xml);
+            }
+            else if (data instanceof Element)
+            {
+                Element ele = (Element) data;
+                writer.output(ele, xml);
+            }
+            else
+            {
+                throw new ConversionException("Data is not a DOM Node"); //$NON-NLS-1$
+            }
+
+            xml.flush();
+
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("var "); //$NON-NLS-1$
+            buffer.append(varname);
+            buffer.append(" = DWREngine._unserializeDocument(\""); //$NON-NLS-1$
+            buffer.append(jsutil.escapeJavaScript(xml.toString()));
+            buffer.append("\");"); //$NON-NLS-1$
+
+            return buffer.toString();
+        }
+        catch (ConversionException ex)
+        {
+            throw ex;
+        }
+        catch (Exception ex)
+        {
+            throw new ConversionException(ex);
+        }
+    }
+
+    /**
+     * The means by which we strip comments
+     */
+    private JavascriptUtil jsutil = new JavascriptUtil();
+}
