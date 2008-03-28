e2b191024f424c5c584ba13a1eef14f0b1232d57
Joe Walker <joe at getahead dot org>
2008-03-28 20:18:58 +0000

    A bunch of changes to simplify things for the screencast

--- jaxer/demo/java/com/example/jaxer/JettyLauncher.java
+++ jaxer/demo/java/com/example/jaxer/JettyLauncher.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.directwebremoting.jaxer.launch;
+package com.example.jaxer;
 
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.nio.SelectChannelConnector;
