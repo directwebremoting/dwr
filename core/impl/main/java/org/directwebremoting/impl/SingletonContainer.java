64ba800d881110dc27079e5e3306206d2f1a243f
Joe Walker <joe at getahead dot org>
2008-04-08 13:46:33 +0000

    upgrading build to be clean - these changes mean that everything compiles cleanly in ant and eclipse

--- core/impl/main/java/org/directwebremoting/impl/SingletonContainer.java
+++ core/impl/main/java/org/directwebremoting/impl/SingletonContainer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2005 Joe Walker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.directwebremoting.impl;
 
 import javax.servlet.ServletConfig;
@@ -8,6 +23,8 @@ import org.directwebremoting.WebContextFactory.WebContextBuilder;
 import org.directwebremoting.annotations.AnnotationsConfigurator;
 import org.directwebremoting.extend.Configurator;
 import org.directwebremoting.extend.ConverterManager;
+import org.directwebremoting.util.FakeServletConfig;
+import org.directwebremoting.util.FakeServletContext;
 
 /**
  * @author Joe Walker [joe at getahead dot ltd dot uk]
