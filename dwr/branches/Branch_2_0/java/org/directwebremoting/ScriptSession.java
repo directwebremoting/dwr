48597a680a19e3579dde67b004f6db5f49536619
joe <joe@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2008-04-21 12:52:19 +0000

    fixed documentation of script session

--- dwr/branches/Branch_2_0/java/org/directwebremoting/ScriptSession.java
+++ dwr/branches/Branch_2_0/java/org/directwebremoting/ScriptSession.java
@@ -42,16 +42,6 @@ public interface ScriptSession
      * Binds an object to this session, using the name specified.
      * If an object of the same name is already bound to the session, the
      * object is replaced.
-     * <p>After this method executes, and if the new object implements
-     * <code>HttpSessionBindingListener</code>, the container calls
-     * <code>HttpSessionBindingListener.valueBound</code>. The container then
-     * notifies any <code>HttpSessionAttributeListener</code>s in the web
-     * application.
-     * <p>If an object was already bound to this session of this name that
-     * implements <code>HttpSessionBindingListener</code>, its
-     * <code>HttpSessionBindingListener.valueUnbound</code> method is called.
-     * <p>If the value passed in is null, this has the same effect as calling
-     * <code>removeAttribute()<code>.
      * @param name the name to which the object is bound; cannot be null
      * @param value the object to be bound
      * @throws IllegalStateException if the page has been invalidated
@@ -62,11 +52,6 @@ public interface ScriptSession
      * Removes the object bound with the specified name from this session.
      * If the session does not have an object bound with the specified name,
      * this method does nothing.
-     * <p>After this method executes, and if the object implements
-     * <code>HttpSessionBindingListener</code>, the container calls
-     * <code>HttpSessionBindingListener.valueUnbound</code>. The container
-     * then notifies any <code>HttpSessionAttributeListener</code>s in the web
-     * application.
      * @param name the name of the object to remove from this session
      * @throws IllegalStateException if the page has been invalidated
      */
@@ -126,7 +111,7 @@ public interface ScriptSession
     /**
      * Returns the last time the client sent a request associated with this
      * session, as the number of milliseconds since 1/1/1970 GMT, and marked by
-     * the time the container recieved the request.
+     * the time the container received the request.
      * <p>Actions that your application takes, such as getting or setting a
      * value associated with the session, do not affect the access time.
      * @return when was this page last accessed, in milliseconds since 1/1/1970 GMT
