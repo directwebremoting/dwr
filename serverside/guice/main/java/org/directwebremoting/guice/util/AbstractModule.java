cc9d19425903c7509a42521f9e96a7bd29da1030
tpeierls <tpeierls@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2008-04-30 14:08:50 +0000

    Worked on javadocs.

--- serverside/guice/main/java/org/directwebremoting/guice/util/AbstractModule.java
+++ serverside/guice/main/java/org/directwebremoting/guice/util/AbstractModule.java
@@ -41,148 +41,29 @@ import org.aopalliance.intercept.MethodInterceptor;
 
 /**
  * An extension of Guice's {@link com.google.inject.AbstractModule AbstractModule}
- * that provides support for member injection of instances constructed at bind-time:
- * the module itself and
+ * that provides support for member injection of objects created before injector creation,
+ * including {@link com.google.inject.AbstractModule AbstractModule} itself and
  * {@link org.aopalliance.intercept.MethodInterceptor MethodInterceptor} instances
- * constructed before injection.
+ * constructed at bind-time.
+ * <p>In addition, there are {@link #bindInterceptor bindInterceptor} variants
+ * that arrange for the interceptors to be constructor-injected, that can wrap each interceptor
+ * with extra common behavior, and that intercept all calls to methods declared by a given type.
+ * These variants all call {@link #registerForInjection registerForInjection}, so there is
+ * no need to call it explicitly when using these methods (though it's harmless to do so).</p>
+ * <p>The {@code Class<?>} and {@code Key<?>} arguments in the
+ * {@link #bindInterceptor bindInterceptor} variants really should
+ * be {@code Class<? extends MethodInterceptor>} and {@code Key<? extends MethodInterceptor>},
+ * but this would cause compile-time warnings on every use.</p>
  * @author Tim Peierls [tim at peierls dot net]
  */
 public abstract class AbstractModule extends com.google.inject.AbstractModule
 {
     /**
-     * Variant of {@link AbstractModule#bindInterceptor bindInterceptor} intercepting non-nested calls
-     * to instances of a given type (or of a subclass of that type), allowing
-     * constructor-injection of interceptors described by class.
+     * No-arg constructor for subclasses.
      */
-    public final void bindInterceptor(Class<?> type,
-                                      Class<?>... classes)
+    protected AbstractModule()
     {
-        bindInterceptor(
-            subclassesOf(type),
-            declaredBy(type),
-            outermostCallWrapper(),
-            classes);
-    }
-
-    /**
-     * Variant of {@link AbstractModule#bindInterceptor bindInterceptor} intercepting non-nested calls
-     * to instances of a given type (or of a subclass of that type), allowing
-     * constructor-injection of interceptors described by {@link com.google.inject.Key Key}.
-     */
-    public final void bindInterceptor(Class<?> type,
-                                      Key<?>... keys)
-    {
-        bindInterceptor(
-            subclassesOf(type),
-            declaredBy(type),
-            outermostCallWrapper(),
-            keys);
-    }
-
-    /**
-     * Variant of {@link #bindInterceptor bindInterceptor} that allows constructor-injection of
-     * interceptors described by class.
-     */
-    public void bindInterceptor(Matcher<? super Class<?>> classMatcher,
-                                Matcher<? super Method> methodMatcher,
-                                Class<?>... classes)
-    {
-        bindInterceptor(classMatcher, methodMatcher, NULL_WRAPPER, classes);
-    }
-
-    /**
-     * Variant of {@link #bindInterceptor bindInterceptor} that allows constructor-injection of
-     * interceptors described by {@link com.google.inject.Key Key}.
-     */
-    public void bindInterceptor(Matcher<? super Class<?>> classMatcher,
-                                Matcher<? super Method> methodMatcher,
-                                Key<?>... keys)
-    {
-        bindInterceptor(classMatcher, methodMatcher, NULL_WRAPPER, keys);
-    }
-
-    /**
-     * Variant of {@link #bindInterceptor bindInterceptor} that allows constructor-injection of
-     * interceptors described by class, each wrapped by a method interceptor wrapper.
-     */
-    public void bindInterceptor(Matcher<? super Class<?>> classMatcher,
-                                Matcher<? super Method> methodMatcher,
-                                MethodInterceptorWrapper wrapper,
-                                Class<?>... classes)
-    {
-        if (classes != null)
-        {
-            MethodInterceptor[] interceptors = new MethodInterceptor[classes.length];
-            int i = 0;
-            for (Class<?> cls : classes)
-            {
-                if (!MethodInterceptor.class.isAssignableFrom(cls))
-                {
-                    addError("bindInterceptor: %s does not implement MethodInterceptor", cls.getName());
-                }
-                else
-                {
-                    @SuppressWarnings("unchecked")
-                    Class<? extends MethodInterceptor> c = (Class<? extends MethodInterceptor>) cls;
-                    interceptors[i++] = wrap(wrapper, c);
-                }
-            }
-            bindInterceptor(classMatcher, methodMatcher, interceptors);
-        }
-    }
-
-    /**
-     * Variant of {@link #bindInterceptor bindInterceptor} that allows constructor-injection of
-     * interceptors described by Key, each wrapped by a method interceptor wrapper.
-     */
-    public void bindInterceptor(Matcher<? super Class<?>> classMatcher,
-                                Matcher<? super Method> methodMatcher,
-                                MethodInterceptorWrapper wrapper,
-                                Key<?>... keys)
-    {
-        if (keys != null)
-        {
-            MethodInterceptor[] interceptors = new MethodInterceptor[keys.length];
-            int i = 0;
-            for (Key<?> key : keys)
-            {
-                Type type = key.getTypeLiteral().getType();
-                if (!(type instanceof Class))
-                {
-                    addError("bindInterceptor: %s is not a Key for a MethodInterceptor subtype", key);
-                }
-                else // type instanceof Class
-                {
-                    Class cls = (Class) type;
-                    if (!MethodInterceptor.class.isAssignableFrom(cls))
-                    {
-                        addError("bindInterceptor: %s does not implement MethodInterceptor", cls.getName());
-                    }
-                    else
-                    {
-                        @SuppressWarnings("unchecked")
-                        Key<? extends MethodInterceptor> k = (Key<? extends MethodInterceptor>) key;
-                        interceptors[i++] = wrap(wrapper, k);
-                    }
-                }
-            }
-            bindInterceptor(classMatcher, methodMatcher, interceptors);
-        }
-    }
-
-    /**
-     * Overridden version of
-     * {@link com.google.inject.AbstractModule#bindInterceptor bindInterceptor}
-     * that, in addition to the standard behavior, arranges for field and method injection
-     * of each MethodInterceptor in {@code interceptors}.
-     */
-    @Override
-    public void bindInterceptor(Matcher<? super Class<?>> classMatcher,
-                                Matcher<? super Method> methodMatcher,
-                                MethodInterceptor... interceptors)
-    {
-        registerForInjection(interceptors);
-        super.bindInterceptor(classMatcher, methodMatcher, interceptors);
+        super();
     }
 
     /**
@@ -196,9 +77,12 @@ public abstract class AbstractModule extends com.google.inject.AbstractModule
 
     /**
      * Arranges for this module and each of the given objects
-     * to be field and method injected when the Injector is created.
+     * to be field and method injected when the
+     * {@link com.google.inject.Injector Injector} is created.
      * It is safe to call this method more than once, and it is safe
      * to call it more than once on the same object(s).
+     * @param objects the objects whose members are to be injected
+     *   when the {@link com.google.inject.Injector Injector} is created
      */
     protected <T> void registerForInjection(T... objects)
     {
@@ -216,11 +100,200 @@ public abstract class AbstractModule extends com.google.inject.AbstractModule
         }
     }
 
+
+    /**
+     * Overridden version of
+     * {@link com.google.inject.AbstractModule#bindInterceptor bindInterceptor}
+     * that, in addition to the standard behavior, arranges for field and method injection
+     * of each MethodInterceptor in {@code interceptors}.
+     * @param classMatcher matches classes the interception should apply to.
+     *   For example: {@code only(Runnable.class)}.
+     * @param methodMatcher matches methods the interception should apply to.
+     *   For example: {@code annotatedWith(Transactional.class)}.
+     * @param methodInterceptors chain of
+     *   {@link org.aopalliance.intercept.MethodInterceptor MethodInterceptor}s
+     *   used to intercept calls.
+     */
+    @Override
+    public void bindInterceptor(Matcher<? super Class<?>> classMatcher,
+                                Matcher<? super Method> methodMatcher,
+                                MethodInterceptor... methodInterceptors)
+    {
+        registerForInjection(methodInterceptors);
+        super.bindInterceptor(classMatcher, methodMatcher, methodInterceptors);
+    }
+
+
+    /**
+     * Variant of {@link #bindInterceptor bindInterceptor} that allows constructor-injection of
+     * interceptors described by class.
+     * @param classMatcher matches classes the interception should apply to.
+     *   For example: {@code only(Runnable.class)}.
+     * @param methodMatcher matches methods the interception should apply to.
+     *   For example: {@code annotatedWith(Transactional.class)}.
+     * @param methodInterceptorClasses chain of
+     *   {@link org.aopalliance.intercept.MethodInterceptor MethodInterceptor}s
+     *   used to intercept calls, specified by class.
+     */
+    public void bindInterceptor(Matcher<? super Class<?>> classMatcher,
+                                Matcher<? super Method> methodMatcher,
+                                Class<?>... methodInterceptorClasses)
+    {
+        bindInterceptor(classMatcher, methodMatcher, NULL_WRAPPER, methodInterceptorClasses);
+    }
+
+    /**
+     * Variant of {@link #bindInterceptor bindInterceptor} that allows constructor-injection of
+     * interceptors described by {@link com.google.inject.Key Key}.
+     * @param classMatcher matches classes the interception should apply to.
+     *   For example: {@code only(Runnable.class)}.
+     * @param methodMatcher matches methods the interception should apply to.
+     *   For example: {@code annotatedWith(Transactional.class)}.
+     * @param methodInterceptorKeys chain of
+     *   {@link org.aopalliance.intercept.MethodInterceptor MethodInterceptor}s
+     *   used to intercept calls, specified by {@link com.google.inject.Key Key}.
+     */
+    public void bindInterceptor(Matcher<? super Class<?>> classMatcher,
+                                Matcher<? super Method> methodMatcher,
+                                Key<?>... methodInterceptorKeys)
+    {
+        bindInterceptor(classMatcher, methodMatcher, NULL_WRAPPER, methodInterceptorKeys);
+    }
+
+
+    /**
+     * Variant of {@link #bindInterceptor bindInterceptor} that allows constructor-injection of
+     * interceptors described by class, each wrapped by a method interceptor wrapper.
+     * @param classMatcher matches classes the interception should apply to.
+     *   For example: {@code only(Runnable.class)}.
+     * @param methodMatcher matches methods the interception should apply to.
+     *   For example: {@code annotatedWith(Transactional.class)}.
+     * @param methodInterceptorWrapper a wrapper applied to each of the specified interceptors.
+     * @param methodInterceptorClasses chain of
+     *   {@link org.aopalliance.intercept.MethodInterceptor MethodInterceptor}s
+     *   used to intercept calls, specified by class.
+     */
+    public void bindInterceptor(Matcher<? super Class<?>> classMatcher,
+                                Matcher<? super Method> methodMatcher,
+                                MethodInterceptorWrapper methodInterceptorWrapper,
+                                Class<?>... methodInterceptorClasses)
+    {
+        if (methodInterceptorClasses != null)
+        {
+            MethodInterceptor[] interceptors = new MethodInterceptor[methodInterceptorClasses.length];
+            int i = 0;
+            for (Class<?> cls : methodInterceptorClasses)
+            {
+                if (!MethodInterceptor.class.isAssignableFrom(cls))
+                {
+                    addError("bindInterceptor: %s does not implement MethodInterceptor", cls.getName());
+                }
+                else
+                {
+                    @SuppressWarnings("unchecked")
+                    Class<? extends MethodInterceptor> c = (Class<? extends MethodInterceptor>) cls;
+                    interceptors[i++] = wrap(methodInterceptorWrapper, c);
+                }
+            }
+            bindInterceptor(classMatcher, methodMatcher, interceptors);
+        }
+    }
+
+    /**
+     * Variant of {@link #bindInterceptor bindInterceptor} that allows constructor-injection of
+     * interceptors described by Key, each wrapped by a method interceptor wrapper.
+     * @param classMatcher matches classes the interception should apply to.
+     *   For example: {@code only(Runnable.class)}.
+     * @param methodMatcher matches methods the interception should apply to.
+     *   For example: {@code annotatedWith(Transactional.class)}.
+     * @param methodInterceptorWrapper a wrapper applied to each of the specified interceptors.
+     * @param methodInterceptorKeys chain of
+     *   {@link org.aopalliance.intercept.MethodInterceptor MethodInterceptor}s
+     *   used to intercept calls, specified by {@link com.google.inject.Key Key}.
+     */
+    public void bindInterceptor(Matcher<? super Class<?>> classMatcher,
+                                Matcher<? super Method> methodMatcher,
+                                MethodInterceptorWrapper methodInterceptorWrapper,
+                                Key<?>... methodInterceptorKeys)
+    {
+        if (methodInterceptorKeys != null)
+        {
+            MethodInterceptor[] interceptors = new MethodInterceptor[methodInterceptorKeys.length];
+            int i = 0;
+            for (Key<?> key : methodInterceptorKeys)
+            {
+                Type type = key.getTypeLiteral().getType();
+                if (!(type instanceof Class))
+                {
+                    addError("bindInterceptor: %s is not a Key for a MethodInterceptor subtype", key);
+                }
+                else // type instanceof Class
+                {
+                    Class cls = (Class) type;
+                    if (!MethodInterceptor.class.isAssignableFrom(cls))
+                    {
+                        addError("bindInterceptor: %s does not implement MethodInterceptor", cls.getName());
+                    }
+                    else
+                    {
+                        @SuppressWarnings("unchecked")
+                        Key<? extends MethodInterceptor> k = (Key<? extends MethodInterceptor>) key;
+                        interceptors[i++] = wrap(methodInterceptorWrapper, k);
+                    }
+                }
+            }
+            bindInterceptor(classMatcher, methodMatcher, interceptors);
+        }
+    }
+
+
+    /**
+     * Variant of {@link #bindInterceptor bindInterceptor} intercepting
+     * non-nested calls to instances of a given type (or of a subclass of that type),
+     * allowing constructor-injection of interceptors described by class.
+     * @param targetType the type for which method calls are to be intercepted
+     * @param methodInterceptorClasses chain of
+     *   {@link org.aopalliance.intercept.MethodInterceptor MethodInterceptor}s
+     *   used to intercept calls, specified by class.
+     */
+    public final void bindInterceptor(Class<?> targetType,
+                                      Class<?>... methodInterceptorClasses)
+    {
+        bindInterceptor(
+            subclassesOf(targetType),
+            declaredBy(targetType),
+            outermostCallWrapper(),
+            methodInterceptorClasses);
+    }
+
+    /**
+     * Variant of {@link #bindInterceptor bindInterceptor} intercepting
+     * non-nested calls to instances of a given type (or of a subclass of that type),
+     * allowing constructor-injection of interceptors described by
+     * {@link com.google.inject.Key Key}.
+     * @param targetType the type for which method calls are to be intercepted
+     * @param methodInterceptorKeys chain of
+     *   {@link org.aopalliance.intercept.MethodInterceptor MethodInterceptor}s
+     *   used to intercept calls, specified by {@link com.google.inject.Key Key}.
+     */
+    public final void bindInterceptor(Class<?> targetType,
+                                      Key<?>... methodInterceptorKeys)
+    {
+        bindInterceptor(
+            subclassesOf(targetType),
+            declaredBy(targetType),
+            outermostCallWrapper(),
+            methodInterceptorKeys);
+    }
+
+
     /**
      * Overrides {@link com.google.inject.AbstractModule#install AbstractModule.install}
      * to automatically register {@code module} for injection if it is an
-     * {@link AbstractModule AbstractModule}, in addition to the standard behavior of
+     * {@link AbstractModule}, in addition to the standard behavior of
      * arranging for {@code module} to be configured during injector creation.
+     * @param module the module to be installed and (if it is an
+     *   {@link AbstractModule}) registered for injection
      */
     @Override
     protected void install(Module module)
