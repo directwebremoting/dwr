d308b50e7bc91151cb4df3d717af550940c7eb13
Joe Walker <joe at getahead dot org>
2008-05-30 15:54:16 +0000

    refactor to move all the jar files into 'lib' directories, and to move core/spi into core/api and split core/convert into core/api and core/impl. This latter change is because java can't separate an implementation dependency from an interface dependency.

---
+++ core/api/main/java/org/directwebremoting/extend/package-info.java
@@ -0,0 +1,13 @@
+
+/**
+This 'extend' package contains many of the classes that are interesting to very
+advanced DWR users, and people embedding or extending DWR.
+
+Files in this package are not subject to the same backwards compatibility ideals
+as the <code>org.directwebremoting</code> package, however the code could change
+in a backwards-incompatible way, so these classes should be avoided unless
+necessary.
+
+@author Joe Walker [joe at getahead dot ltd dot uk]
+*/
+package org.directwebremoting.extend;
