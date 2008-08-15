658e9d62c9a2ea7003a8d9df91b35c7b3cf5644d
Joe Walker <joe at getahead dot org>
2008-08-15 19:43:55 +0000

    Adding the concept of a OutputStreamLoader so we can create a FileTransfer object with an object with the ability write to an OutputStream instead of needing a byte[] or an InputStream.

--- core/api/main/java/org/directwebremoting/io/FileTransfer.java
+++ core/api/main/java/org/directwebremoting/io/FileTransfer.java
@@ -16,7 +16,9 @@
 package org.directwebremoting.io;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * One of the 2 ways you can receive uploaded files from a DWR enabled page is
@@ -31,6 +33,20 @@ public class FileTransfer
      * A ctor for the 3 things browsers tell us about the uploaded file
      * @param name The remote source filename
      * @param mimeType The mime type passed in by the browser
+     * @param outputStreamLoader A means by which the data can be read
+     */
+    public FileTransfer(String name, String mimeType, OutputStreamLoader outputStreamLoader)
+    {
+        this.name = name;
+        this.mimeType = mimeType;
+        this.inputStream = null;
+        this.outputStreamLoader = outputStreamLoader;
+    }
+
+    /**
+     * A ctor for the 3 things browsers tell us about the uploaded file
+     * @param name The remote source filename
+     * @param mimeType The mime type passed in by the browser
      * @param inputStream A means by which the data can be read
      */
     public FileTransfer(String name, String mimeType, InputStream inputStream)
@@ -38,6 +54,7 @@ public class FileTransfer
         this.name = name;
         this.mimeType = mimeType;
         this.inputStream = inputStream;
+        this.outputStreamLoader = null;
     }
 
     /**
@@ -51,11 +68,11 @@ public class FileTransfer
         this.name = name;
         this.mimeType = mimeType;
         this.inputStream = new ByteArrayInputStream(data);
+        this.outputStreamLoader = null;
     }
 
     /**
-     * Returns the content type passed by the browser or null if not defined. 
-     * @return The content type passed by the browser or null if not defined.
+     * Returns the content type passed by the browser or null if not defined.
      */
     public String getMimeType()
     {
@@ -63,8 +80,8 @@ public class FileTransfer
     }
 
     /**
-     * Returns an InputStream that can be used to retrieve the contents of the file. 
-     * @return An InputStream that can be used to retrieve the contents of the file.
+     * Returns an InputStream that can be used to retrieve the contents of the
+     * file.
      */
     public InputStream getInputStream()
     {
@@ -72,8 +89,17 @@ public class FileTransfer
     }
 
     /**
+     * Returns an InputStream that can be used to retrieve the contents of the
+     * file.
+     */
+    public OutputStreamLoader getOutputStreamLoader()
+    {
+        return outputStreamLoader;
+    }
+
+    /**
      * Returns the original filename in the client's file-system, as provided by
-     * the browser (or other client software). 
+     * the browser (or other client software).
      * In most cases, this will be the base file name, without path information.
      * However, some clients, such as the Opera browser, do include path
      * information.
@@ -85,17 +111,36 @@ public class FileTransfer
     }
 
     /**
+     * Sometimes you might have a way to write to an {@link OutputStream} and
+     * don't want to create a temporary in memory buffer to hold the data before
+     * it is squirted to the browser.
+     * @author Joe Walker [joe at getahead dot ltd dot uk]
+     */
+    public interface OutputStreamLoader
+    {
+        /**
+         * Write all the data to the given output stream.
+         */
+        public void load(OutputStream out) throws IOException;
+    }
+
+    /**
      * The remote source filename
      */
-    private String name;
+    private final String name;
 
     /**
      * The mime type passed in by the browser
      */
-    private String mimeType;
+    private final String mimeType;
 
     /**
      * A means by which the data can be read
      */
-    private InputStream inputStream;
+    private final InputStream inputStream;
+
+    /**
+     * A means by which the data can be written
+     */
+    private final OutputStreamLoader outputStreamLoader;
 }
