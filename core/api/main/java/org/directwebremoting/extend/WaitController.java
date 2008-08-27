2042aae8d7c4b82f79fbc728b29b2f6b6b6d0f94
Joe Walker <joe at getahead dot org>
2008-08-27 15:48:02 +0000

    Added TODO comment about naming

--- core/api/main/java/org/directwebremoting/extend/WaitController.java
+++ core/api/main/java/org/directwebremoting/extend/WaitController.java
@@ -18,6 +18,7 @@ package org.directwebremoting.extend;
 /**
  * A WaitController allows the {@link ServerLoadMonitor} to know what is waiting
  * and enables it to tell them to stop waiting.
+ * TODO: Change the name of this to something that indicates it's about shutdown
  * @author Joe Walker [joe at getahead dot ltd dot uk]
  */
 public interface WaitController
