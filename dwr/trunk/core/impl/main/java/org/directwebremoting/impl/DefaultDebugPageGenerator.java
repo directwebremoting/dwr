9cb0917d2a3a47f518df2728886b88da4737eef6
joe <joe@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2008-08-27 10:17:50 +0000

    We had utilities to copy data from in to out in LocalUtil and CopyUtils. They should all be in CopyUtils.

--- dwr/trunk/core/impl/main/java/org/directwebremoting/impl/DefaultDebugPageGenerator.java
+++ dwr/trunk/core/impl/main/java/org/directwebremoting/impl/DefaultDebugPageGenerator.java
@@ -19,6 +19,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringWriter;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collection;
@@ -37,6 +38,7 @@ import org.directwebremoting.extend.DwrConstants;
 import org.directwebremoting.servlet.EngineHandler;
 import org.directwebremoting.servlet.PathConstants;
 import org.directwebremoting.servlet.UtilHandler;
+import org.directwebremoting.util.CopyUtils;
 import org.directwebremoting.util.JavascriptUtil;
 import org.directwebremoting.util.LocalUtil;
 
@@ -309,7 +311,9 @@ public class DefaultDebugPageGenerator implements DebugPageGenerator
                     BufferedReader in = new BufferedReader(new InputStreamReader(raw));
                     try
                     {
-                        output = LocalUtil.readFully(in);
+                        StringWriter writer = new StringWriter();
+                        CopyUtils.copy(in, writer);
+                        output = writer.toString();
                     }
                     catch (IOException ex)
                     {
