f391bcd36c97509884a18fa796de2a43dfae8db4
joe <joe@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2008-08-27 15:48:38 +0000

    WebworkUtilHandler should be in the serverside/webwork module

--- dwr/trunk/serverside/webwork/main/java/org/directwebremoting/webwork/WebworkUtilHandler.java
+++ dwr/trunk/serverside/webwork/main/java/org/directwebremoting/webwork/WebworkUtilHandler.java
@@ -13,9 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.directwebremoting.servlet;
+package org.directwebremoting.webwork;
 
 import org.directwebremoting.extend.DwrConstants;
+import org.directwebremoting.servlet.FileJavaScriptHandler;
 
 /**
  * A Handler that supports requests for DWRActionUtil.js
