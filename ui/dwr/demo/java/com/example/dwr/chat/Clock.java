f2b0b60920db5851c7fd3a75324275f135284589
Joe Walker <joe at getahead dot org>
2008-08-28 16:20:13 +0000

    Working on dwr.war to simplify and make it more fit for a release includes removing unused classes, trimming the less good demos, and adding documentation

--- ui/dwr/demo/java/com/example/dwr/chat/Clock.java
+++ ui/dwr/demo/java/com/example/dwr/chat/Clock.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.dwr.clock;
+package com.example.dwr.chat;
 
 import java.util.Date;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
