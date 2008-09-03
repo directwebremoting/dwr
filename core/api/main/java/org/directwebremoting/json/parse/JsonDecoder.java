2880a39f26f329699876fe41fc35c1f29c778f92
Joe Walker <joe at getahead dot org>
2008-09-03 23:14:11 +0000

    quick document this before anyone notices ;-)

--- core/api/main/java/org/directwebremoting/json/parse/JsonDecoder.java
+++ core/api/main/java/org/directwebremoting/json/parse/JsonDecoder.java
@@ -18,74 +18,99 @@ package org.directwebremoting.json.parse;
 import java.math.BigDecimal;
 
 /**
+ * Used by {@link JsonParser} to allow the parse process to be mostly stateless,
+ * and to abstract the process of creating objects. It is very likely that
+ * {@link org.directwebremoting.json.parse.impl.AbstractJsonDecoder} will be
+ * an easier start point than this.
+ * TODO: Some naming tidy-up and thought around when the various addNumber methods are used
  * @author Joe Walker [joe at getahead dot ltd dot uk]
  */
 public interface JsonDecoder<T>
 {
+    /**
+     * When the parse is finished, this gives access to the result
+     */
     T getRoot() throws JsonParseException;
 
     /**
-     *
+     * We have encountered a {. The first thing to happen inside an object will
+     * be an {@link #addString(String)} so we do need to remember that we are
+     * inside an object.
+     * <p>
+     * In the future we might change the {@link #addString(String)} call to be
+     * addPropertyName() or similar.
      */
     void beginObject() throws JsonParseException;
 
     /**
-     *
+     * We have encountered a }
      */
     void endObject() throws JsonParseException;
 
     /**
-     *
+     * We have encountered a :. This happens directly after an
+     * {@link #addString(String)} and which happens after {@link #beginObject()}
+     * Following this one of the add methods will be called for the data behind
+     * the property, followed by {@link #endMember()}
      */
     void beginMember() throws JsonParseException;
 
     /**
-     *
+     * @see #beginMember()
      */
     void endMember() throws JsonParseException;
 
     /**
-     *
+     * We have encountered a [. What follows is a series of addX() calls and
+     * then an {@link #endArray()}.
      */
     void beginArray() throws JsonParseException;
 
     /**
-     *
+     * We have encountered a ].
+     * @see #beginArray()
      */
     void endArray() throws JsonParseException;
 
     /**
-     * @param value
+     * Add a string member. See the note on {@link #beginObject()}
      */
     void addString(String value) throws JsonParseException;
 
     /**
-     * @param value
-     */
-    void addNumber(BigDecimal value) throws JsonParseException;
-
-    /**
-     * @param value
-     */
-    void addDouble(double value) throws JsonParseException;
-
-    /**
-     * @param value
-     */
-    void addLong(long value) throws JsonParseException;
-
-    /**
-     * @param value
+     * Add an integer member. This method will always be used for numbers unless
+     * the number will not fit into an integer.
+     * @see #addLong(long)
      */
     void addInt(int value) throws JsonParseException;
 
     /**
-     * @param value
+     * Add an long member. After {@link #addInt(int)} this method will be used
+     * for numbers unless the number will not fit into an integer.
+     * @see #addDouble(double)
+     */
+    void addLong(long value) throws JsonParseException;
+
+    /**
+     * Add a double member. After {@link #addLong(long)} this method will be
+     * used for numbers unless the number will not fit into a double.
+     * @see #addNumber(BigDecimal)
+     */
+    void addDouble(double value) throws JsonParseException;
+
+    /**
+     * Add a BigDecimal member. After {@link #addDouble(double)} this method
+     * will be used for numbers.
+     */
+    void addNumber(BigDecimal value) throws JsonParseException;
+
+    /**
+     * Add a boolean member
      */
     void addBoolean(boolean value) throws JsonParseException;
 
     /**
-     *
+     * Add a null member
      */
     void addNull() throws JsonParseException;
 }
