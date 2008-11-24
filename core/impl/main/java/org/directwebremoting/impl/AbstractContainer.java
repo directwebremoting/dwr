0d3d8ee801737965797256ade87c7b178703f60a
Joe Walker <joe at getahead dot org>
2008-11-24 21:52:13 +0000

    trivial ignore - an earlier cleanup caused a formatting change

--- core/impl/main/java/org/directwebremoting/impl/AbstractContainer.java
+++ core/impl/main/java/org/directwebremoting/impl/AbstractContainer.java
@@ -16,6 +16,7 @@
 package org.directwebremoting.impl;
 
 import java.util.Collection;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.directwebremoting.Container;
