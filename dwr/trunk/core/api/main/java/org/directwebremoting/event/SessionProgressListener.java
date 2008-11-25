a491f095a2f16c95d57aee3b650763c41b621dff
joe <joe@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2008-11-25 15:20:37 +0000

    Fix for DWR-311 (SessionProgressListener does not implement Serializable) See http://bugs.directwebremoting.org/bugs//browse/DWR-311 http://www.nabble.com/FileUpload-with-DwrSpringServlet-tp20653656p20681880.html Also added some documentation

--- dwr/trunk/core/api/main/java/org/directwebremoting/event/SessionProgressListener.java
+++ dwr/trunk/core/api/main/java/org/directwebremoting/event/SessionProgressListener.java
@@ -15,35 +15,54 @@
  */
 package org.directwebremoting.event;
 
+import java.io.Serializable;
+
 import javax.servlet.http.HttpSession;
+
 import org.apache.commons.fileupload.ProgressListener;
 
 /**
  * Progress listener that stores results in the user session.
  * @author Jose Noheda
  */
-public class SessionProgressListener implements ProgressListener
+public class SessionProgressListener implements ProgressListener, Serializable
 {
     public SessionProgressListener(HttpSession session)
     {
         this.session = session;
     }
 
+    /**
+     * Accessor for the number of bytes read, as passed in by
+     * {@link ProgressListener#update(long, long, int)}
+     */
     public long getBytesRead()
     {
         return bytesRead;
     }
 
+    /**
+     * The total number of bytes, which are being read. May be -1, if this
+     * number is unknown. Passed in by
+     * {@link ProgressListener#update(long, long, int)}
+     */
     public long getContentLength()
     {
         return contentLength;
     }
 
+    /**
+     * The number of the field, which is currently being read. (0 = no item so
+     * far, 1 = first item is being read, ...)
+     */
     public long getItem()
     {
         return item;
     }
 
+    /* (non-Javadoc)
+     * @see org.apache.commons.fileupload.ProgressListener#update(long, long, int)
+     */
     public void update(long newBytesRead, long newContentLength, int items)
     {
         if (session.getAttribute(CANCEL_UPLOAD) != null)
@@ -62,10 +81,18 @@ public class SessionProgressListener implements ProgressListener
     private volatile HttpSession session;
 
     /**
-     * The progress.
+     * @see SessionProgressListener#getBytesRead()
      */
     private volatile long bytesRead = 0L;
+
+    /**
+     * @see #getContentLength()
+     */
     private volatile long contentLength = 0L;
+
+    /**
+     * @see #getItem()
+     */
     private volatile long item = 0L;
 
     /**
