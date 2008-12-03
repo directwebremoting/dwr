f3b5949a1218119f23fd31f941b3b593ec203e6d
mikewse <mikewse@users.noreply.github.com>
2008-12-03 19:43:09 +0000

    DWR-316: importNode if null/undefined test

--- ui/dwr/main/java/org/directwebremoting/ui/servlet/util.js
+++ ui/dwr/main/java/org/directwebremoting/ui/servlet/util.js
@@ -1485,7 +1485,7 @@ dwr.util._importNode = function(doc, importedNode, deep) {
       }
     }
 
-    if (typeof importedNode.style != "undefined") {
+    if (importedNode.style != null) {
       newNode.style.cssText = importedNode.style.cssText;
     }
   }
