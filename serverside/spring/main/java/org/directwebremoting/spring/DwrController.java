bc19de5598c5cb4bf2c3a3294c563ccd1d2488b4
Joe Walker <joe at getahead dot org>
2009-02-04 14:06:51 +0000

    Allowing correct shutdown when using tomcat+spring+reverse ajax

--- serverside/spring/main/java/org/directwebremoting/spring/DwrController.java
+++ serverside/spring/main/java/org/directwebremoting/spring/DwrController.java
@@ -35,6 +35,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.util.Assert;
@@ -99,7 +100,7 @@ import org.springframework.web.servlet.mvc.AbstractController;
  * @author Joe Walker [joe at getahead dot ltd dot uk]
  * @author Bram Smeets
  */
-public class DwrController extends AbstractController implements BeanNameAware, InitializingBean, BeanFactoryAware
+public class DwrController extends AbstractController implements BeanNameAware, InitializingBean, DisposableBean, BeanFactoryAware
 {
     /**
      * Is called by the Spring container to set the bean factory. <br/>
@@ -209,6 +210,15 @@ public class DwrController extends AbstractController implements BeanNameAware, 
                 webContextBuilder.disengageThread();
             }
         }
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.beans.factory.DisposableBean#destroy()
+     */
+    public void destroy() throws Exception
+    {
+        container.servletDestroyed();
+        container.contextDestroyed();
     }
 
     /**
