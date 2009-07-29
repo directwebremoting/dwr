a51bdeccf14ecc96aba665e2f28ebc4c6fc6dca7
David Marginian <dmarginian@users.noreply.github.com>
2009-07-29 02:48:15 +0000

    Demo fixes.

--- ui/dwr/demo/web/json/jQuery/simple/text.js
+++ ui/dwr/demo/web/json/jQuery/simple/text.js
@@ -1,7 +1,7 @@
 
  function update() {
 	  var name = dwr.util.getValue("demoName");
-	  $.post("../../dwr/jsonp/Demo/sayHello/" + name, { },
+	  $.post("../../../dwr/jsonp/Demo/sayHello/" + name, { },
 			  function(data) {
 		        dwr.util.setValue("demoReply", data.reply);
       }, "jsonp"); 
