8be31b08ddcd94ccc8715ba5f5f148421db46dbd
davidmarginian <davidmarginian@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2009-07-29 03:43:40 +0000

    Demo fixes.

--- dwr/trunk/ui/dwr/demo/web/json/dojo/simple/text.js
+++ dwr/trunk/ui/dwr/demo/web/json/dojo/simple/text.js
@@ -2,7 +2,7 @@ function update() {
    var name = dojo.byId("demoName").value;
    dojo.xhrGet({
    // The following URL must match that used to test the server.
-      url: "../dwr/jsonp/Demo/sayHello/" + name,
+      url: "../../../dwr/jsonp/Demo/sayHello/" + name,
       handleAs: "json",
       load: function(responseObject, ioArgs) {
          // Now you can just use the object
