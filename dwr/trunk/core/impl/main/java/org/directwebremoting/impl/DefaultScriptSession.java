29383394d0dbe9b66e6b67e0b13874055424c6c3
davidmarginian <davidmarginian@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2009-12-01 05:08:43 +0000

    DWR-427 - Concurrency Issues.

--- dwr/trunk/core/impl/main/java/org/directwebremoting/impl/DefaultScriptSession.java
+++ dwr/trunk/core/impl/main/java/org/directwebremoting/impl/DefaultScriptSession.java
@@ -446,7 +446,6 @@ public class DefaultScriptSession implements RealScriptSession
         }
 
         DefaultScriptSession that = (DefaultScriptSession) obj;
-
         return this.id.equals(that.id);
 
     }
