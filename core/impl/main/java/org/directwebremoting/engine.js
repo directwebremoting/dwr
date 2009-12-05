4f721b64dd719475622d9e67627031cd20384f52
David Marginian <dmarginian@users.noreply.github.com>
2009-12-05 19:32:46 +0000

    DWR-433 - Offer the possibility to configure the session cookie name (other than JSESSIONID).

--- core/impl/main/java/org/directwebremoting/engine.js
+++ core/impl/main/java/org/directwebremoting/engine.js
@@ -1973,10 +1973,10 @@ if (typeof dwr == 'undefined') dwr = {};
         urlBuffer.push(".dwr");
       }
       // Play nice with url re-writing
-       var sessionMatchExpr = eval("/" + dwr.engine._sessionCookieName + "=([^?#]+)/"); 
+       var sessionMatchExpr = eval("/" + dwr.engine._sessionCookieName.toLowerCase() + "=([^?#]+)/"); 
 	   var sessionMatch = location.href.match(sessionMatchExpr); 
 	   if (sessionMatch != null) { 
-	       urlBuffer.push(";" + dwr.engine._sessionCookieName + "="); 
+	       urlBuffer.push(";" + dwr.engine._sessionCookieName.toLowerCase() + "="); 
 	       urlBuffer.push(sessionMatch[1]); 
 	   } 
 
