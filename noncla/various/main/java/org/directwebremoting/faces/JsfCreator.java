2ee84f8038bdd25e557c815fcfa585dec115735b
David Marginian <dmarginian@users.noreply.github.com>
2010-12-09 04:38:55 +0000

    Some JSF updates to support JSF 2 without deprecated calls.

--- noncla/various/main/java/org/directwebremoting/faces/JsfCreator.java
+++ noncla/various/main/java/org/directwebremoting/faces/JsfCreator.java
@@ -20,45 +20,26 @@ import javax.faces.context.FacesContext;
 import javax.faces.el.ValueBinding;
 import javax.faces.el.VariableResolver;
 
-import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.Log;
-import org.directwebremoting.extend.AbstractCreator;
+import org.apache.commons.logging.LogFactory;
 import org.directwebremoting.extend.Creator;
-import org.directwebremoting.util.LocalUtil;
 
 /**
  * This is a DWR creator implementation, to allow dwr beans to be allocated into
- * JSF scopes and into jeffs3 specific scope (i.e. the flow scope)
+ * JSF scopes and into jeffs3 specific scope (i.e. the flow scope).
+ * JSF 1.2 and below.
+ *
  * @author Pierpaolo Follia
  * @author Joe Walker [joe at getahead dot ltd dot uk]
  * @author Vinicius Melo [vdmelo at gmail dot com]
  */
-public class JsfCreator extends AbstractCreator implements Creator
+public class JsfCreator extends AbstractJsfCreator implements Creator
 {
-    /* (non-Javadoc)
-     * @see org.directwebremoting.Creator#getType()
-     */
-    public Class<?> getType()
-    {
-        if (instanceType == null)
-        {
-            try
-            {
-                instanceType = getInstance().getClass();
-            }
-            catch (InstantiationException ex)
-            {
-                log.error("Failed to instansiate object to detect type.", ex);
-                return Object.class;
-            }
-        }
-
-        return instanceType;
-    }
 
     /* (non-Javadoc)
      * @see org.directwebremoting.Creator#getInstance()
      */
+    @Override
     public Object getInstance() throws InstantiationException
     {
         FacesContext facesContext = FacesContext.getCurrentInstance();
@@ -87,66 +68,6 @@ public class JsfCreator extends AbstractCreator implements Creator
 
         return resolvedObject;
     }
-
-    /**
-     * Determine whether String is a value binding expression or not.
-     * @param expression The expression to test for value bindingness
-     * @return true if the expression contains a VB expression
-     */
-    public static boolean isVBExpression(String expression)
-    {
-        if (expression == null)
-        {
-            return false;
-        }
-
-        int start = expression.indexOf("#{");
-        int end = expression.indexOf('}');
-
-        return start != -1 && start < end;
-    }
-
-    /**
-     * @return Returns the managedBeanName.
-     */
-    public String getManagedBeanName()
-    {
-        return managedBeanName;
-    }
-
-    /**
-     * @param managedBeanName The managedBeanName to set.
-     */
-    public void setManagedBeanName(String managedBeanName)
-    {
-        this.managedBeanName = managedBeanName;
-    }
-
-    /**
-     * What sort of class do we create?
-     * @param classname The name of the class
-     */
-    public void setClass(String classname)
-    {
-        try
-        {
-            this.instanceType = LocalUtil.classForName(classname);
-        }
-        catch (ClassNotFoundException ex)
-        {
-            throw new IllegalArgumentException("Creator.ClassNotFound");
-        }
-    }
-
-    /**
-     * The name of the bean to get from the FacesContext
-     */
-    private String managedBeanName;
-
-    /**
-     * The cached type of bean that we are creating
-     */
-    private Class<?> instanceType;
 
     /**
      * The log stream
