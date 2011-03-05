bdc1f507685e2f4cdff9823b136a2b29db363853
davidmarginian <davidmarginian@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2011-03-05 16:38:28 +0000

    Maven work.

---
+++ dwr/trunk/demo/content/dojo/store/storeDemo.js
@@ -0,0 +1,8 @@
+
+dojo.provide("custom.storeDemo");
+
+dojo.require("dojox.grid.DataGrid");
+dojo.require("dojo.parser");
+
+dojo.require("dijit.layout.TabContainer");
+dojo.require("dijit.layout.ContentPane");
