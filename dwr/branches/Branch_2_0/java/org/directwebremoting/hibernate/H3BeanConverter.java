85bd555657534f482169f374c097a07351b5f638
davidmarginian <davidmarginian@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2011-09-07 20:48:08 +0000

    DWR-537 - Synchronization issue with H3BeanConverter.

--- dwr/branches/Branch_2_0/java/org/directwebremoting/hibernate/H3BeanConverter.java
+++ dwr/branches/Branch_2_0/java/org/directwebremoting/hibernate/H3BeanConverter.java
@@ -35,191 +35,181 @@ import org.hibernate.proxy.LazyInitializer;
 
 /**
  * BeanConverter that works with Hibernate to get BeanInfo.
+ * 
  * @author Joe Walker [joe at getahead dot ltd dot uk]
  */
-public class H3BeanConverter extends BeanConverter implements Converter
-{
-    /* (non-Javadoc)
-     * @see org.directwebremoting.convert.BeanConverter#getPropertyMapFromObject(java.lang.Object, boolean, boolean)
-     */
-    public Map getPropertyMapFromObject(Object example, boolean readRequired, boolean writeRequired) throws MarshallException
-    {
-        Class clazz = getClass(example);
+public class H3BeanConverter extends BeanConverter implements Converter {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.directwebremoting.convert.BeanConverter#getPropertyMapFromObject(
+	 * java.lang.Object, boolean, boolean)
+	 */
+	public Map getPropertyMapFromObject(Object example, boolean readRequired,
+			boolean writeRequired) throws MarshallException {
+		Class clazz = getClass(example);
 
-        try
-        {
-            BeanInfo info = Introspector.getBeanInfo(clazz);
-            PropertyDescriptor[] descriptors = info.getPropertyDescriptors();
+		try {
+			BeanInfo info = Introspector.getBeanInfo(clazz);
+			PropertyDescriptor[] descriptors = info.getPropertyDescriptors();
 
-            Map properties = new HashMap();
-            for (int i = 0; i < descriptors.length; i++)
-            {
-                PropertyDescriptor descriptor = descriptors[i];
-                String name = descriptor.getName();
+			Map properties = new HashMap();
+			for (int i = 0; i < descriptors.length; i++) {
+				PropertyDescriptor descriptor = descriptors[i];
+				String name = descriptor.getName();
 
-                // We don't marshall getClass()
-                if (name.equals("class"))
-                {
-                    continue;
-                }
+				// We don't marshall getClass()
+				if (name.equals("class")) {
+					continue;
+				}
 
-                // And this is something added by hibernate
-                if (name.equals("hibernateLazyInitializer"))
-                {
-                    continue;
-                }
+				// And this is something added by hibernate
+				if (name.equals("hibernateLazyInitializer")) {
+					continue;
+				}
 
-                // Access rules mean we might not want to do this one
-                if (!isAllowedByIncludeExcludeRules(name))
-                {
-                    continue;
-                }
+				// Access rules mean we might not want to do this one
+				if (!isAllowedByIncludeExcludeRules(name)) {
+					continue;
+				}
 
-                if (readRequired && descriptor.getReadMethod() == null)
-                {
-                    continue;
-                }
+				if (readRequired && descriptor.getReadMethod() == null) {
+					continue;
+				}
 
-                if (writeRequired && descriptor.getWriteMethod() == null)
-                {
-                    continue;
-                }
+				if (writeRequired && descriptor.getWriteMethod() == null) {
+					continue;
+				}
 
-                if (!assumeSession)
-                {
-                    // We don't marshall un-initialized properties for
-                    // Hibernate3
-                    String propertyName = descriptor.getName();
-                    Method method = findGetter(example, propertyName);
+				if (!assumeSession) {
+					// We don't marshall un-initialized properties for
+					// Hibernate3
+					String propertyName = descriptor.getName();
+					Method method = findGetter(example, propertyName);
 
-                    if (method == null)
-                    {
-                        log.warn("Failed to find property: " + propertyName);
+					if (method == null) {
+						log.warn("Failed to find property: " + propertyName);
 
-                        properties.put(name, new PlainProperty(propertyName, null));
-                        continue;
-                    }
+						properties.put(name, new PlainProperty(propertyName,
+								null));
+						continue;
+					}
 
-                    if (!Hibernate.isPropertyInitialized(example, propertyName))
-                    {
-                        properties.put(name, new PlainProperty(propertyName, null));
-                        continue;
-                    }
+					if (!Hibernate.isPropertyInitialized(example, propertyName)) {
+						properties.put(name, new PlainProperty(propertyName,
+								null));
+						continue;
+					}
 
-                    // This might be a lazy-collection so we need to double check
-                    Object retval = method.invoke(example, new Object[] {});
-                    if (!Hibernate.isInitialized(retval))
-                    {
-                        properties.put(name, new PlainProperty(propertyName, null));
-                        continue;
-                    }
-                }
+					// This might be a lazy-collection so we need to double
+					// check
+					Object retval = method.invoke(example, new Object[] {});
+					if (!Hibernate.isInitialized(retval)) {
+						properties.put(name, new PlainProperty(propertyName,
+								null));
+						continue;
+					}
+				}
 
-                properties.put(name, new H3PropertyDescriptorProperty(descriptor));
-            }
+				properties.put(name, new H3PropertyDescriptorProperty(
+						descriptor));
+			}
 
-            return properties;
-        }
-        catch (Exception ex)
-        {
-            throw new MarshallException(clazz, ex);
-        }
-    }
+			return properties;
+		} catch (Exception ex) {
+			throw new MarshallException(clazz, ex);
+		}
+	}
 
-    /**
-     * Hibernate makes {@link Class#getClass()} diffficult ...
-     * @param example The class that we want to call {@link Class#getClass()} on
-     * @return The type of the given object
-     */
-    public Class getClass(Object example)
-    {
-        if (example instanceof HibernateProxy)
-        {
-            HibernateProxy proxy = (HibernateProxy) example;
-            LazyInitializer initializer = proxy.getHibernateLazyInitializer();
-            SessionImplementor implementor = initializer.getSession();
+	/**
+	 * Hibernate makes {@link Class#getClass()} diffficult ...
+	 * 
+	 * @param example
+	 *            The class that we want to call {@link Class#getClass()} on
+	 * @return The type of the given object
+	 */
+	public Class getClass(Object example) {
+		if (example instanceof HibernateProxy) {
+			HibernateProxy proxy = (HibernateProxy) example;
+			LazyInitializer initializer = proxy.getHibernateLazyInitializer();
+			SessionImplementor implementor = initializer.getSession();
 
-            if (initializer.isUninitialized())
-            {
-                try
-                {
-                    // getImplementation is going to want to talk to a session
-                    if (implementor.isClosed())
-                    {
-                        // Give up and return example.getClass();
-                        return example.getClass();
-                    }
-                }
-                catch (NoSuchMethodError ex)
-                {
-                    // We must be using Hibernate 3.0/3.1 which doesn't have
-                    // this method
-                }
-            }
+			if (initializer.isUninitialized()) {
+				try {
+					// getImplementation is going to want to talk to a session
+					if (implementor.isClosed()) {
+						// Give up and return example.getClass();
+						return example.getClass();
+					}
+				} catch (NoSuchMethodError ex) {
+					// We must be using Hibernate 3.0/3.1 which doesn't have
+					// this method
+				}
+			}
 
-            return initializer.getImplementation().getClass();
-        }
-        else
-        {
-            return example.getClass();
-        }
-    }
+			return initializer.getImplementation().getClass();
+		} else {
+			return example.getClass();
+		}
+	}
 
-    /**
-     * Cache the method if possible, using the classname and property name to
-     * allow for similar named methods.
-     * @param data The bean to introspect
-     * @param property The property to get the accessor for
-     * @return The getter method
-     * @throws IntrospectionException
-     */
-    protected Method findGetter(Object data, String property) throws IntrospectionException
-    {
-        String key = data.getClass().getName() + ":" + property;
-        Method method = null;
-        synchronized (methods) {
-            method = (Method) methods.get(key);
-        }
-        if (method == null)
-        {
-            PropertyDescriptor[] props = Introspector.getBeanInfo(data.getClass()).getPropertyDescriptors();
-            for (int i = 0; i < props.length; i++)
-            {
-                if (props[i].getName().equalsIgnoreCase(property))
-                {
-                    method = props[i].getReadMethod();
-                }
-            }
-            synchronized (methods) {
-                methods.put(key, method);
-            }
-        }
+	/**
+	 * Cache the method if possible, using the classname and property name to
+	 * allow for similar named methods.
+	 * 
+	 * @param data
+	 *            The bean to introspect
+	 * @param property
+	 *            The property to get the accessor for
+	 * @return The getter method
+	 * @throws IntrospectionException
+	 */
+	protected Method findGetter(Object data, String property)
+			throws IntrospectionException {
+		String key = data.getClass().getName() + ":" + property;
+		Method method = null;
+		synchronized (methods) {
+			method = (Method) methods.get(key);
+		}
+		if (method == null) {
+			PropertyDescriptor[] props = Introspector.getBeanInfo(
+					data.getClass()).getPropertyDescriptors();
+			for (int i = 0; i < props.length; i++) {
+				if (props[i].getName().equalsIgnoreCase(property)) {
+					method = props[i].getReadMethod();
+				}
+			}
+			synchronized (methods) {
+				methods.put(key, method);
+			}
+		}
 
-        return method;
-    }
+		return method;
+	}
 
-    /**
-     * @param assumeSession the assumeSession to set
-     */
-    public void setAssumeSession(boolean assumeSession)
-    {
-        this.assumeSession = assumeSession;
-    }
+	/**
+	 * @param assumeSession
+	 *            the assumeSession to set
+	 */
+	public void setAssumeSession(boolean assumeSession) {
+		this.assumeSession = assumeSession;
+	}
 
-    /**
-     * Do we assume there is an open session and read properties?
-     */
-    protected boolean assumeSession = false;
+	/**
+	 * Do we assume there is an open session and read properties?
+	 */
+	protected boolean assumeSession = false;
 
-    /**
-     * The cache of method lookups that we've already done
-     * <p>GuardedBy("self") for iteration and compound actions
-     */
-    protected final Map methods = new HashMap();
+	/**
+	 * The cache of method lookups that we've already done
+	 * <p>
+	 * GuardedBy("self") for iteration and compound actions
+	 */
+	protected final Map methods = new HashMap();
 
-    /**
-     * The log stream
-     */
-    private static final Logger log = Logger.getLogger(H3BeanConverter.class);
+	/**
+	 * The log stream
+	 */
+	private static final Logger log = Logger.getLogger(H3BeanConverter.class);
 }
-
