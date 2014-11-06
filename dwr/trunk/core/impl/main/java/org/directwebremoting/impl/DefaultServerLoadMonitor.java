6f21649da3d2b49ee37c84d913ae7159d91648b5
mikewse <mikewse@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2014-11-06 20:23:03 +0000

    make connectedTime and disconnectedTime configurable for Full Streaming Mode

--- dwr/trunk/core/impl/main/java/org/directwebremoting/impl/DefaultServerLoadMonitor.java
+++ dwr/trunk/core/impl/main/java/org/directwebremoting/impl/DefaultServerLoadMonitor.java
@@ -42,10 +42,28 @@ public class DefaultServerLoadMonitor extends AbstractServerLoadMonitor
     }
 
     /**
+     * Setter to allow configuration from the container (f ex init-param)
+     * @param connectedTime
+     */
+    public void setConnectedTime(long connectedTime)
+    {
+        this.connectedTime = connectedTime;
+    }
+
+    /**
+     * Setter to allow configuration from the container (f ex init-param)
+     * @param disconnectedTime
+     */
+    public void setDisconnectedTime(int disconnectedTime)
+    {
+        this.disconnectedTime = disconnectedTime;
+    }
+
+    /**
      * The time we are currently waiting before sending a browser away and
      * asking it to reconnect.
      */
-    protected int connectedTime = 60000;
+    protected long connectedTime = 60000;
 
     /**
      * How long are we telling users to wait before they come back next
