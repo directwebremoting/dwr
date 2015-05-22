80d9d83b72b9994e7f00e7496b96bdc3a1b70311
mikewse <mikewse@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2015-05-22 13:18:07 +0000

    DWR-587: added contentDispositionType parameter to control downloads

--- dwr/trunk/ui/dwr/demo/web/simple/download.js
+++ dwr/trunk/ui/dwr/demo/web/simple/download.js
@@ -2,7 +2,15 @@
 function downloadPdfFile() {
   var pdftext = dwr.util.getValue('pdftext');
 
-  UploadDownload.downloadPdfFile(pdftext, function(data) {
-    dwr.engine.openInDownload(data);
+  UploadDownload.downloadPdfFile(pdftext, function(url) {
+    dwr.engine.openInDownload(url);
+  });
+}
+
+function showPdfFile() {
+  var pdftext = dwr.util.getValue('pdftext');
+
+  UploadDownload.downloadPdfFile(pdftext, function(url) {
+    window.location = url + "?contentDispositionType=inline";
   });
 }
