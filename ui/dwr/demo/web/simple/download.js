03d8215c84d382ab02800da19864230aa47f956f
mikewse <mikewse@users.noreply.github.com>
2015-05-27 21:20:07 +0000

    ie7 download fixes (make async to workaround popup blocker)

--- ui/dwr/demo/web/simple/download.js
+++ ui/dwr/demo/web/simple/download.js
@@ -2,15 +2,17 @@
 function downloadPdfFile() {
   var pdftext = dwr.util.getValue('pdftext');
 
-  UploadDownload.downloadPdfFile(pdftext, function(url) {
-    dwr.engine.openInDownload(url);
+  UploadDownload.downloadPdfFile(pdftext, {
+    callback: function(url) { dwr.engine.openInDownload(url); },
+    async: false // workaround IE7/8's aggressive popup blocker
   });
 }
 
 function showPdfFile() {
   var pdftext = dwr.util.getValue('pdftext');
 
-  UploadDownload.downloadPdfFile(pdftext, function(url) {
-    window.location = url + "?contentDispositionType=inline";
+  UploadDownload.downloadPdfFile(pdftext, {
+    callback: function(url) { window.location = url + "?contentDispositionType=inline"; },
+    async: false // workaround IE7/8's aggressive popup blocker
   });
 }
