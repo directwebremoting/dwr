dcf34c2a44e5d2ebf2a705a5e81504495a053e76
davidmarginian <davidmarginian@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2015-10-01 03:05:35 +0000

    https://directwebremoting.atlassian.net/browse/DWR-651

--- dwr/trunk/serverside/spring/main/java/org/directwebremoting/spring/namespace/AnnotationScannerParser.java
+++ dwr/trunk/serverside/spring/main/java/org/directwebremoting/spring/namespace/AnnotationScannerParser.java
@@ -24,6 +24,7 @@ import org.directwebremoting.annotations.GlobalFilter;
 import org.directwebremoting.annotations.RemoteProxy;
 import org.directwebremoting.spring.DwrClassPathBeanDefinitionScanner;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.DefaultBeanNameGenerator;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
@@ -53,6 +54,10 @@ public class AnnotationScannerParser implements BeanDefinitionParser
     {
         ClassPathBeanDefinitionScanner scanner = new DwrClassPathBeanDefinitionScanner(parserContext.getRegistry());
         String basePackage = element.getAttribute("base-package");
+        // Override - By default Spring uses a name generator that uses AnnotationBeanNameGenerator which uses the name specified on the Component
+        // annotation and if not present uses the simple name.  Since our annotation-scanner doesn't scan @Component if two classes with the
+        // same simple name exist in different packages there will be issues.  See https://directwebremoting.atlassian.net/browse/DWR-651.
+        scanner.setBeanNameGenerator(new DefaultBeanNameGenerator());
         if (!hasText(basePackage))
         {
             if (log.isInfoEnabled())
